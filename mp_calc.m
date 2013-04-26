18c1b06787d624c1d02569ec2aedb063514d9445
Karol Auguštin <karol@augustin.pl>
2013-04-26 12:14:21 +0200

    Add GPL license header to source files

--- mp_calc.m
+++ mp_calc.m
@@ -1,3 +1,27 @@
+%    MP MATLAB plugin
+%
+%    Copyright (C) 2013 Tomasz Spustek, Konrad Kwaśkiewicz, Karol Auguštin
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%    Tomasz Spustek <tomasz@spustek.pl>
+%    Konrad Kwaśkiewicz <konrad.kwaskiewicz@gmail.com>
+%    Karol Auguštin <karol@augustin.pl>
+
+
+
+
 function [reconstructing_array parameters_array] = mp_calc(EEG,channel_nr,epoch_nr,minS,maxS,dE,energy,iter,nfft)
 
 if (channel_nr > EEG.nbchan) || (channel_nr < 1)
